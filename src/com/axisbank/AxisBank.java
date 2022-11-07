511201faa29516eb9ae344cb0fed0c4a73e9f300
agkvignesh <agkvignesh42@gmail.com>
2022-11-07 21:46:32 +0530

    AAAAAAA

--- src/com/axisbank/AxisBank.java
+++ src/com/axisbank/AxisBank.java
@@ -1,13 +1,17 @@
 package com.axisbank;
 
 public class AxisBank {
-public static void main(String[] args) {
-	int studentId = 1001,studentMark = 80;
-	
-	if(studentId>=1000&&studentMark<=60) {
-		System.out.println("anna universcity sudent");
-	}else {
-		System.out.println("pass");
+	private void Second() {
+		System.out.println("Second");
 	}
-}
+
+	public static void main(String[] args) {
+		int studentId = 1001, studentMark = 80;
+
+		if (studentId >= 1000 && studentMark <= 60) {
+			System.out.println("anna universcity sudent");
+		} else {
+			System.out.println("pass");
+		}
+	}
 }
